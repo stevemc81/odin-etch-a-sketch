d6d891bd26bae93e930d1cc5696c611f7f0c44e8
Steven McWee <stevenmcwee@gmail.com>
2025-09-08 17:31:33 +0100

    Allow custom grid size to be chosen

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
+const gridSizeBtn = document.querySelector("#grid-size-btn");
 const container = document.querySelector(".container");
 
-for (let i = 0; i < (16 * 16); i++) {
-    const box = document.createElement("div");
-    box.classList.add("grid-square");
-    box.addEventListener("mouseover", () => {
-        box.style.backgroundColor = "black";
-    })
-    container.appendChild(box);
+createGrid(16);
+
+gridSizeBtn.addEventListener("click", () => {
+    const gridSize = parseInt(prompt("Enter a whole number from 16 to 100 for the grid size:"));
+
+    if (gridSize < 16 || gridSize > 100 || !Number.isInteger(gridSize)) {
+        alert("Please enter a whole number between 16 and 100!")
+    } else {
+        createGrid(gridSize);
+    }
+});
+
+function createGrid(size) {
+    container.textContent = "";
+    for (let i = 0; i < (size * size); i++) {
+        const box = document.createElement("div");
+        box.classList.add("grid-square");
+        box.style.width = `${600 / size}px`;
+        box.style.height = `${600 / size}px`;
+        box.addEventListener("mouseover", () => {
+            box.style.backgroundColor = "black";
+        });
+        container.appendChild(box);
+    }
 }
