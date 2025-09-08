760e7882e601fd57d284329b318d133bec24950b
Steven McWee <stevenmcwee@gmail.com>
2025-09-08 15:49:54 +0100

    Change div background colour on hover

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ const container = document.querySelector(".container");
 for (let i = 0; i < (16 * 16); i++) {
     const box = document.createElement("div");
     box.classList.add("grid-square");
+    box.addEventListener("mouseover", () => {
+        box.style.backgroundColor = "black";
+    })
     container.appendChild(box);
 }
